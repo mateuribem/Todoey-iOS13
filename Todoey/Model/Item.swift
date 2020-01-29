174191b4f5fda5e027091faf432e34bce0b33ef7
Mateo <mateuribem@gmail.com>
2020-01-29 18:49:35 -0500

    Create Item Model

---
+++ Todoey/Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Mateo Uribe Moreno on 1/29/20.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+
+}
