5469e838fc9b31f240e10275ef2f267ed6954028
Mateo <mateuribem@gmail.com>
2020-01-29 16:31:37 -0500

    Create UITableViewController and Setup required methods with hardcorded code

---
+++ Todoey/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Todoey
+//
+//  Created by Mateo Uribe Moreno on 1/29/20.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct K {
+    static let cellIdentifier = "ToDoItemCell"
+}
