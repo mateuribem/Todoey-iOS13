02b56d6285777495e45d6c0aee9744e22e7c5aaa
Mateo <mateuribem@gmail.com>
2020-02-05 00:36:18 -0500

    Create new entity Category

---
+++ Todoey/Controller/CategoryTableViewController.swift
@@ -0,0 +1,24 @@
+//
+//  CategoryTableViewController.swift
+//  Todoey
+//
+//  Created by Mateo Uribe Moreno on 2/2/20.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class CategoryTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+ 
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+    }
+    
+
+}
